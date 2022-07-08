e61e2258ce8ac4c215f564a36e2b6bfc90925605
Ghukas Buniatyan <g.buniatyan09@gmail.com>
2022-07-08 12:35:12 +0400

    UniversityApp

---
+++ UniversityAppp/UniversityAppp.Services/ITeacherService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityAppp.Models;
+
+namespace UniversityAppp.Services
+{
+    public interface ITeacherService
+    {
+        IEnumerable<Teacher>? GetAll();
+        Teacher? Get(Guid id);
+        void Create(Teacher? student);
+        void Update(Teacher? student);
+        void Delete(Guid id);
+    }
+}
---
+++ UniversityAppp/UniversityAppp.Services/TeacherService.cs
@@ -0,0 +1,35 @@
+﻿using UniversityAppp.Models;
+namespace UniversityAppp.Services
+{
+    internal class TeacherService : ITeacherService
+    {
+        static List<Teacher> _teachers = new List<Teacher>();
+
+        public void Create(Teacher? teacher)
+        {
+            if (teacher != null)
+                _teachers.Add(teacher);
+        }
+
+        public void Delete(Guid id)
+        {
+            _teachers.RemoveAll(x => x.Id == id);
+        }
+
+        public Teacher? Get(Guid id)
+            => _teachers.FirstOrDefault(x => x.Id == id);
+
+        public IEnumerable<Teacher>? GetAll()
+            => _teachers;
+
+        public void Update(Teacher? teacher)
+        {
+            Teacher? currentTeacher = _teachers.FirstOrDefault(s => s.Id == teacher?.Id);
+            int index = -1;
+            if (currentTeacher != null)
+                index = _teachers.IndexOf(currentTeacher);
+            if (teacher != null)
+                _teachers[index] = teacher;
+        }
+    }
+}
